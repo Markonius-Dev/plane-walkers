9eebb7fcf94412f6d441f116fdf648387458b6cf
Markonius <markonius.dev@gmail.com>
2022-04-29 19:03:39 +0200

    🔬 New grid with accessible cells and unavailable cells

--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class GridController : MonoBehaviour
 {
+    private GridLayout gridLayout;
     [SerializeField] private GameObject grid;
     private List<GameObject> entities;
 
+    private List<Vector3Int> cells;
+
     void Start()
     {
-        GridLayout gridLayout = GetComponent<GridLayout>();
+        gridLayout = GetComponent<GridLayout>();
         Debug.Log($"Cell Gap : {gridLayout.cellGap.y}");
 
         entities = new List<GameObject>(0);
@@ -19,12 +22,44 @@ public class GridController : MonoBehaviour
         character.transform.parent = transform;
         entities.Add(character);
 
-        
+        cells = new List<Vector3Int>();
+
+        cells.Add(new Vector3Int(0, 0, 0));
+        cells.Add(new Vector3Int(1, 0, 0));
+        cells.Add(new Vector3Int(-1, 0, 0));
+        cells.Add(new Vector3Int(-2, 0, 0));
+        cells.Add(new Vector3Int(0, 1, 0));
+        cells.Add(new Vector3Int(1, 1, 0));
+        cells.Add(new Vector3Int(-1, 1, 0));
+        cells.Add(new Vector3Int(-2, 1, 0));
+        cells.Add(new Vector3Int(0, -1, 0));
+        cells.Add(new Vector3Int(1, -1, 0));
+        cells.Add(new Vector3Int(-1, -1, 0));
+        cells.Add(new Vector3Int(-2, -1, 0));
+        cells.Add(new Vector3Int(0, -2, 0));
+        cells.Add(new Vector3Int(1, -2, 0));
+        cells.Add(new Vector3Int(-1, -2, 0));
+        cells.Add(new Vector3Int(-2, -2, 0));
+
     }
 
-    
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0)) DebugLogCell(Input.mousePosition);
+    }
+
+    private void DebugLogCell(Vector2 touchPos)
+    {
+        Vector2 worldTouchPos = Camera.main.ScreenToWorldPoint(touchPos);
+        Vector3Int cell = gridLayout.WorldToCell(worldTouchPos);
+        Debug.Log($"{cell}");
+        if (ReturnCellExists(cell)) Debug.Log($"The cell is in the board");
+        else Debug.Log($"The cell isn't in the board");
+    }
+
+    private bool ReturnCellExists(Vector3Int cell)
+    {
+        return cells.Exists(el => (el.x == cell.x && el.y == cell.y));
     }
 }
