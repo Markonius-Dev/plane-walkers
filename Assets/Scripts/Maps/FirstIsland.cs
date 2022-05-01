ff8daa62ee2af0ddcfc3e9228b7a2239a0ea06d4
Markonius <markonius.dev@gmail.com>
2022-05-01 02:51:31 +0200

    📄 Create entity object

--- Assets/Scripts/Maps/FirstIsland.cs
+++ Assets/Scripts/Maps/FirstIsland.cs
@@ -6,24 +6,48 @@ public class FirstIsland : MonoBehaviour
 {
     // Script managing First Island map.
 
-    private List<GameObject> entities;
+    private List<Entity> entities;
 
     private void Start()
     {
-        entities = new List<GameObject>();
+        entities = new List<Entity>();
 
         SpawnCharacter();
 
         void SpawnCharacter()
         {
             Debug.Log($"Loading Character");
-            GameObject character = Instantiate(Resources.Load<GameObject>("Prefabs/CharacterSample"));
+            Entity character = new Entity();
 
-            character.transform.parent = transform;
-            character.transform.name = "Character";
+            character.gameObject = Instantiate(Resources.Load<GameObject>("Prefabs/CharacterSample"));
+
+            character.gameObject.transform.parent = transform;
+            character.gameObject.transform.name = "Character";
+
+
 
             if (entities.Count == 0) entities.Add(character);
             else entities[0] = character;
+        }
+    }
+
+    public class Entity
+    {
+        public GameObject gameObject;
+        public Vector3Int position;
+        public bool moving;
+        public int direction;
+        public float movementDelta;
+        public List<int> movementQueue;
+
+        public Entity()
+        {
+            gameObject = null;
+            position = new Vector3Int(0, 0, 0);
+            moving = false;
+            direction = 7;
+            movementDelta = 0;
+            movementQueue = new List<int>();
         }
     }
 }
